43fafba0b48befd38c51ea3648160a8bf1fa9e2c
Fish <fishagbalo@gmail.com>
2025-01-06 23:28:07 +0800

    Adding Basic Count UseState

---
+++ src/hooks/use-state/count.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+export const Count = () => {
+    const [count, setCount] = useState(0);
+const increment = () => setCount(count + 1);
+const decrement = () => setCount(count - 1);
+
+  return (
+    <div><p>Count: {count}</p>
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+    </div>
+  )
+}
--- src/hooks/use-state/index.jsx
+++ src/hooks/use-state/index.jsx
@@ -1,9 +1,11 @@
 import { TodoList } from "./todo-list";
+import { Count } from "./count";
 export const UseState = () => {
 
 return (
   <div>
-   <TodoList></TodoList>
+   {/* <TodoList></TodoList> */}
+   <Count></Count>
   </div>
 );
 };
