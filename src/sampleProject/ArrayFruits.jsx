5c3c1d1e0b0a524ac82f9130d659aef88c7cdce5
Fish <fishagbalo@gmail.com>
2025-01-15 20:20:07 +0800

    Adding Array with fruits

--- src/sampleProject/ArrayFruits.jsx
+++ src/sampleProject/ArrayFruits.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
 function ArrayFruits() {
+    const [foods, setFoods] = React.useState(["Apple", "Banana", 
+        "Cherry", "Date", "Elderberry", "Fig", 
+        "Grape", "Honeydew", "Jackfruit", 
+        "Kiwi", "Lemon", "Mango", "Nectarine", 
+        "Orange", "Papaya", "Quince", "Raspberry"])
+
+    function handleAddFood() {
+        const foodInput = document.getElementById('foodInput')
+        const newFood = foodInput.value
+        if (newFood === '') {
+            alert('Please enter a food name')
+            return
+        }
+        setFoods(f => [...f, newFood])
+        foodInput.value = ''
+    }
+
+    function handleRemoveFood(index) {
+        setFoods(f => f.filter((_, i) => i !== index))
+                            //  ^ ingnored
+    }
   return (
-    <div>ArrayFruits</div>
+    <div>
+        <h2>List of Food</h2>
+        <ul>
+            {foods.map((food, index) => (
+                <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>
+            ))}
+        </ul>
+        <div>
+            <input type="text" id="foodInput" placeholder='Enter food name' />
+            <button onClick={handleAddFood}>Add Food</button>
+        </div>
+    </div>
   )
 }
 
