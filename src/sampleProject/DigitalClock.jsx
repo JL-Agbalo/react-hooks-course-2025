47d6f16310b009717c7ab6474bd4d40276b23a1c
Fish <fishagbalo@gmail.com>
2025-01-16 20:56:43 +0800

    `Refactor DigitalClock component to use functional component and update time display`

--- src/sampleProject/DigitalClock.jsx
+++ src/sampleProject/DigitalClock.jsx
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react';
 
-export class DigitalClock extends Component {
-  render() {
-    return (
-      <div>DigitalClock</div>
-    )
-  }
+function DigitalClock() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    function formatTime() {
+        let hours = time.getHours();
+        const minitus = time.getMinutes();
+        const seconds = time.getSeconds();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+
+        hours = hours % 12 || 12;
+
+        return `${padZero(hours)}:${padZero(minitus)}:${padZero(seconds)} ${ampm}`;
+    }
+
+    function padZero(number){
+        return number < 10 ? '0' + number : number
+
+    }
+
+  return (
+    <div>
+        <div className='name'>
+            <span>{formatTime()}</span>
+        </div>
+    </div>
+  )
 }
 
 export default DigitalClock
